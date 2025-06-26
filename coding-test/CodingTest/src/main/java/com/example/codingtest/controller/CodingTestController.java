10394806aca729daa424964ebfb59573456c806c
kxh1127a <kxh1127a@gmail.com>
2025-06-26 11:25:43 +0900

    add

--- coding-test/CodingTest/src/main/java/com/example/codingtest/controller/CodingTestController.java
+++ coding-test/CodingTest/src/main/java/com/example/codingtest/controller/CodingTestController.java
@@ -130,5 +130,9 @@ public class CodingTestController {
         public List<Map<String, Object>> getTest4() {
         return scoreService.getAll();
     }
+    @GetMapping("/all/a4")
+            public List<Map<String, Object>> getTest4() {
+            return scoreService.getAll();
+    }
 }
 
